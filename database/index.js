48082bd9151fb3fc1fe5d66b21b7f30ef7977b60
hebertsanto <hebertsantos0704@gmail.com>
2023-11-26 08:49:22 -0300

    some change in style code

--- database/index.js
+++ database/index.js
@@ -4,16 +4,16 @@ require('dotenv').config();
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 
-const url_database = `mongodb+srv://${user}:${password}@cluster0.1eud56w.mongodb.net/?retryWrites=true&w=majority`
+const url_database = `mongodb+srv://${user}:${password}@cluster0.1eud56w.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(url_database, ({ useNewUrlParser : true }));
 
-mongoose.connection.on('connected', () => {
-    console.log('tudo certo')
-})
+mongoose.connection.on('connected', (sucess) => {
+    console.log(`everything was successful ${sucess}`);
+});
 
 mongoose.connection.on('error', (error) => {
-    console.log('deu erro', error)
-})
+    console.log(`some error ocurred, ${ error } `)
+});
 
 module.exports = mongoose.connection;
--- index.js
+++ index.js
@@ -11,15 +11,15 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(routes)
 
-const port = process.env.port;
-
 db.on('conected', () => {
     console.log('successfully connected')
 })
 db.on('error', (error) => {
-    console.log('there was an error connecting', error)
+    console.log(`error connecting ${error}`)
 });
 
+const port = process.env.port;
+
 app.listen(port, () => {
-    console.log('tudo ok')
+    console.log(`sever is running on port ${ port }`);
 })
