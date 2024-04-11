e541d0f1d47e85abce2a1dfe7f639c4b286a17b7
hebertsanto <hebertsantos0704@gmail.com>
2024-04-10 22:55:17 -0300

    feat : Add authentication middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { authMiddleware } from './middlewares/auht-middlware';
 const app = express();
 
 import routes from './routes';
@@ -14,6 +15,7 @@ import { env } from './env';
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(authMiddleware);
 app.use(routes);
 
 db.on('conected', () => {
