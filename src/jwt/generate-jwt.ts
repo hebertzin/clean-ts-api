8754349c2cb32f61098a54ec3fced925a5ce6785
hebertsanto <hebertsantos0704@gmail.com>
2024-04-11 00:10:43 -0300

    docs : add jsdocs in some files

--- src/jwt/generate-jwt.ts
+++ src/jwt/generate-jwt.ts
@@ -1,20 +1,37 @@
 import jwt from 'jsonwebtoken';
 import { config } from 'dotenv';
 import mongoose from 'mongoose';
+import { env } from '../env';
+import { logger } from '../logger';
 
 config();
+
+/**
+ * @param id
+ * @returns token
+ * @throws error
+ */
 
 export const generateJwt = async (id: mongoose.Types.ObjectId | undefined) => {
   try {
     const token = jwt.sign(
       { id },
-      process.env.SECRET_JWT || '075bc8899ea9f527b763ab37fceeff5f',
+      env.SECRET_JWT || '075bc8899ea9f527b763ab37fceeff5f',
       {
         expiresIn: '1d',
       },
     );
-    return token.toString();
+
+    /*
+        This token will be used to log the user into the system,
+        without it, the user will not have access to resources
+    */
+    return token;
   } catch (error) {
-    return error;
+    logger.log({
+      level: 'error',
+      message: 'Some error ocurred trying generate token',
+    });
+    throw error;
   }
 };
--- src/utils/generate-hash.ts
+++ src/utils/generate-hash.ts
@@ -1,5 +1,10 @@
 import bcrypt from 'bcrypt';
+import { logger } from '../logger';
 
+/**
+ * @param password
+ * @returns hash of password to save in database
+ */
 export const passwordHash = async (password: string) => {
   try {
     if (!password) {
@@ -7,6 +12,10 @@ export const passwordHash = async (password: string) => {
     }
     return await bcrypt.hash(password, 10);
   } catch (error) {
+    logger.log({
+      level: 'error',
+      message: 'Some error ocurred trying generate hash of password',
+    });
     return error;
   }
 };
