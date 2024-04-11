b405679379a0b6a7b5064f74d10cd4ca35967fa5
hebertsanto <hebertsantos0704@gmail.com>
2024-04-10 23:10:08 -0300

    chore : add  and config winston to project

---
+++ src/logger.ts
@@ -0,0 +1,11 @@
+import { createLogger, transports, format } from 'winston';
+
+export const logger = createLogger({
+  level: 'info',
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
