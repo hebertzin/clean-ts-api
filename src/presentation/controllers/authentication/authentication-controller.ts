13bb1e6af988fd89e9e8ee8f4eff6ee2e0296acc
hebertsanto <hebertsantos0704@gmail.com>
2024-09-30 02:05:24 -0300

    feat : add controllers

---
+++ src/presentation/controllers/authentication/authentication-controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { Controller, HttpResponse } from '../../../domain/controllers';
+import { Authentication } from '../../../domain/authentication';
+import { HttpStatusCode } from '../../../utils/http-status-code';
+
+export class AuthController implements Controller {
+  constructor(private authenticationUseCase: Authentication) {}
+  async handle(req: Request): Promise<HttpResponse> {
+    try {
+      const token = await this.authenticationUseCase.auth(req.body.email, req.body.password);
+      return { msg: 'Authentication was made successfully', statusCode: HttpStatusCode.Ok, body: token };
+    } catch (error) {
+      return { msg: error.msg, statusCode: error.code };
+    }
+  }
+}
