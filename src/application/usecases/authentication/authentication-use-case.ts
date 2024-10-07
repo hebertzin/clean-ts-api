e3c702312523232c84bfb79e239c6c02d682e3ca
hebertsanto <hebertsantos0704@gmail.com>
2024-10-07 11:20:04 -0300

    refactor : add payload jwt

--- src/application/usecases/authentication/authentication-use-case.ts
+++ src/application/usecases/authentication/authentication-use-case.ts
@@ -5,24 +5,28 @@ import { HttpStatusCode } from '../../../utils/http-status-code';
 import { Authentication } from '../../../domain/authentication';
 import { Token } from '../../../domain/generate-token';
 
+export type Payload = {
+  email: string;
+  password: string;
+};
+
 export class AuthenticationUseCase implements Authentication {
   constructor(
     private readonly loadByEmailRepository: LoadUserByEmailRepository,
     private readonly jwt: Token,
     private readonly hasherComparer: HashComparer,
   ) {}
-  async auth(email: string, password: string): Promise<string> {
-    const user = await this.loadByEmailRepository.loadByEmail(email);
+  async auth(payload: Payload): Promise<string> {
+    const user = await this.loadByEmailRepository.loadByEmail(payload.email);
     if (!user) {
       throw new UserDoesNotExist('User does not exists', HttpStatusCode.NotFound);
     }
-    const isMatch = await this.hasherComparer.compare(user.password, password);
+    const isMatch = await this.hasherComparer.compare(user.password, payload.password);
     if (!isMatch) {
       throw new CredentialsError('Invalid Credentials', HttpStatusCode.Unauthorized);
     }
     try {
-      const token = await this.jwt.generateToken(user.email, password);
-      return token;
+      return await this.jwt.generateToken(user.email, payload.password);
     } catch (e) {
       throw new AppError('Some error has been ocurred making login', HttpStatusCode.InternalServerError);
     }
--- src/presentation/controllers/authentication/authentication-controller.ts
+++ src/presentation/controllers/authentication/authentication-controller.ts
@@ -7,7 +7,7 @@ export class AuthController implements Controller {
   constructor(private authenticationUseCase: Authentication) {}
   async handle(req: Request): Promise<HttpResponse> {
     try {
-      const token = await this.authenticationUseCase.auth(req.body.email, req.body.password);
+      const token = await this.authenticationUseCase.auth({ email: req.body.email, password: req.body.password });
       return { msg: 'Authentication was made successfully', statusCode: HttpStatusCode.Ok, body: token };
     } catch (error) {
       return { msg: error.msg, statusCode: error.code };
